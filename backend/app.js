547180d755759eb86cd606bca952d4f5fb4691d3
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2022-09-29 23:10:29 +0200

    Starting project

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+// On place dans ce fichier notre application Express
+
+const express = require('express'); // on importe Express 
+
+const app = express(); // constante qui va permettre de créer une application Express
+
+module.exports = app; // permet d'accéder à l'application depuis nos autres fichiers du projet (notamment le server)
+
+app.use ((req, res, next)=>{ // Middleware : Afin de répondre à la requête qui émise par le serveur vers l'application. Permet de récupérer un objet au format JSON.Sans cette réponse, POSTMAN nous renvoi une erreur 404 lors de la requête GET
+    res.json({message : 'Votre requête a bien été reçu'})
+    next() // Next permet de terminer la requête et passé au middleware suivant 
+})
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const app = require ('./app') // importer le fichier app.js sur le fichier serveur.js
+
+app.set('port',process.env.PORT || 3000 ) // Permet de dire à l'application express sur quel port du serveur elle va tourner et la déployer
+
+const server = http.createServer(app); // appelle de la fonction app, pour appeler l'application dans la serveur
+
+server.listen(process.env.PORT || 3000); // permet d'écouter (déployer) le serveur surle port 300
+
+// Utilisation de mongoose (Mongo DB) sur le projet
+
+const mongoose = require ('mongoose')
+
+// Nous précisons sur quel DB nous allons travailler
+
+mongoose.connect('mongodb://localhost/blog', function(err) {
+  if (err) { throw err; }
+});
+
+// Permet de se déconnecter de MongoDB
+
+mongoose.connection.close();

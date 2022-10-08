bcc7ef429ee4ca3dee0faca8792d52dd41622e5b
Mohamed <mohamed.ebarhmatin@yahoo.fr>
2022-10-08 21:28:46 +0200

    Multer configuration ok

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+// Storage est passé à multer, afin d'indiquer où enregistrer les images. filename indique à multer d'utiliser le nom d'origine, de remplacer les espaces par des underscores et d'ajouter un timestamp Date.now() comme nom de fichier. Elle utilise ensuite la constante dictionnaire de type MIME pour résoudre l'extension de fichier appropriée.
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'images');
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
+});
+
+module.exports = multer({storage: storage}).single('image');
--- backend/models/thing.js
+++ backend/models/thing.js
@@ -3,8 +3,19 @@ const mongoose = require('mongoose');
 
 // On indique ici le type de données et leur carctère (obligatoire ou non)
 const thingSchema = mongoose.Schema({
+    userId : {type : String, required : true},
+    name : {type : String, required : true},
+    manufacturer : {type : String, required : true},
+    description : {type : String, required : true},
+    mainPepper : {type : String, required : true},
+    imageUrl : {type : String, required : true},
+    heat : {type : Number, required : true},
+    likes : {type : Number, required : true},
+    dislikes : {type : Number, required : true},
+    usersLiked : {type : ["String <userId>"], required : true},
+    usersDisliked : {type : ["String <userId>"], required : true},
     sauce : {type : String, required : true},
-    file : {type : String, required : true}
+    file : {type : String, required : true},
 });
 
 //on exporte le model correspondant afin de les utiliser et interagir avec la base de données
